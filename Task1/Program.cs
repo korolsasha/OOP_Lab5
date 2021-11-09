92fb87d3a309f0c17d8a617705b7a3e3fab52d9d
Oleksandr Korol <koros.korol2003@gmail.com>
2021-11-09 22:25:28 +0200

    Create 2 new classes in Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,6 +10,30 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            Console.InputEncoding = Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Что будет происходить с квадратом при нажатии стрелки влево/вправо?");
+            Console.WriteLine("1. Перемещение");
+            Console.WriteLine("2. Поворот");
+            Console.WriteLine();
+            Console.Write("Ваш выбор: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            SquareMove move = null;
+
+            if (choice == 1)
+            {
+                move = new SquareMove();
+            }
+            else if (choice == 2)
+            {
+                move = new SquareRotate();
+            }
+            else { Console.WriteLine("Неверный выбор."); }
+
+            move.ReadParametrs();
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Task1/SquareMove.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class SquareMove
+    {
+        public void ReadParametrs()
+        {
+            Console.Write("x: ");
+            float x = float.Parse(Console.ReadLine());
+
+            Console.Write("y: ");
+            float y = float.Parse(Console.ReadLine());
+
+            Console.Write("Длинна стороны: ");
+            float side = float.Parse(Console.ReadLine());
+        }
+
+
+    }
+}
